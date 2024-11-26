1a5e658ad2e23241343bf2b260014bb20dc7960a
ShiraSt1 <shiristern24@gmail.com>
2024-11-26 23:21:31 +0200

    add letter

--- server/gim.js
+++ server/gim.js
@@ -2,6 +2,7 @@ const letters=
 [
     {letter:'a', num:1 },
     {letter:'b', num:2 },
-    {letter:'c', num:3 }
+    {letter:'c', num:3 },
+    {letter:'d', num:4 }
 ]
 module.exports = { letters }
